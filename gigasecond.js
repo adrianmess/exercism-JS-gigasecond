0d7700c1f34db22f575552ef1c8c95c7a8eabf5c
Adrian Messado <adrian.messado@gmail.com>
2019-04-16 18:34:20 -0700

    use getTime to add milliseconds to date, test all

--- gigasecond.js
+++ gigasecond.js
@@ -1,3 +1,7 @@
-export const gigasecond = () => {
-
+export const gigasecond = (dateOrigin) => {
+	// must use getTime as getSeconds only works with 0-60 seconds
+	// getTime works with miliseconds from 1970 to present day
+	// convert gigaseconds to milliseconds and use getTime
+	const milisec = 1000000000000;
+	return new Date(dateOrigin.getTime() + milisec);
 }
